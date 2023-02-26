26b0b6e6a7e52d2c55aba2795a8888352a91777a
Mellissa Santos <114121324+santos-mellissa@users.noreply.github.com>
2023-02-26 02:06:13 -0300

    Delete script_2_data_nascimento.py

--- script_2_data_nascimento.py
+++
@@ -1,5 +0,0 @@
-# Ler o dia, mês e ano de nascimento e mostrar data formatada.
-dia = input("Qual o dia de seu nascimento? ")
-mes = input("Qual o mês de seu nascimento? ")
-ano = input("Qual o ano de seu nascimento? ")
-print("Você nasceu no dia " + dia + " de " + mes + " de " + ano + ".")
