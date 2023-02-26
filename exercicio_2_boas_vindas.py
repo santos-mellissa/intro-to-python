ef3ceb3faf8fc2e44361dd299b7995189c150c2e
Mellissa Santos <114121324+santos-mellissa@users.noreply.github.com>
2023-02-26 02:07:10 -0300

    Add files via upload

---
+++ exercicio_2_boas_vindas.py
@@ -0,0 +1,4 @@
+# Crie um programa que receba um nome e retorne uma mensagem de boas-vindas.
+
+nome = input("Qual é o seu nome? ")
+print("É um prazer te conhecer {}!".format(nome)) # Outra opção seria `print("É um prazer te conhecer " + nome + "!")`
