b7996cd5ce59d1b85c41f07a29e179a773f06161
Mellissa Santos <114121324+santos-mellissa@users.noreply.github.com>
2023-02-25 21:58:01 -0300

    Add files via upload

---
+++ script_3_soma.py
@@ -0,0 +1,11 @@
+#Recebe dois números e retorna a soma deles.
+
+#Versão 1
+num_a = int(input("Digite o primeiro número: "))
+num_b = int(input("Digite o segundo número: "))
+print("A soma é:", num_a + num_b)
+
+#Versão 2
+num_a = input("Digite o primeiro número: ")
+num_b = input("Digite o segundo número: ")
+print("A soma é:", int(num_a) + int(num_b))
