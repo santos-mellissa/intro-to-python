37d52b6d5992fb1735d9df8ad0305bcfea06ddf1
Mellissa Santos <114121324+santos-mellissa@users.noreply.github.com>
2023-02-27 00:30:18 -0300

    Update script_3_soma.py

--- script_3_soma.py
+++ script_3_soma.py
@@ -1,11 +1,6 @@
-#Recebe dois números e retorna a soma deles.
+# Crie um programa que leia dois números e mostre a soma entre eles.
 
-#Versão 1
-num_a = int(input("Digite o primeiro número: "))
-num_b = int(input("Digite o segundo número: "))
-print("A soma é:", num_a + num_b)
+num_a = int(input('Digite o primeiro número: '))
+num_b = int(input('Digite o segundo número: '))
 
-#Versão 2
-num_a = input("Digite o primeiro número: ")
-num_b = input("Digite o segundo número: ")
-print("A soma é:", int(num_a) + int(num_b))
+print('A soma entre {} e {} é: {} .'.format(num_a, num_b,num_a + num_b))
