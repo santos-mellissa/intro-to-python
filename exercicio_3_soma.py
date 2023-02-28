821d2f33d9771d4a3f3dfbe5a6d9f4b494ed2ec7
Mellissa Santos <114121324+santos-mellissa@users.noreply.github.com>
2023-02-28 12:45:16 -0300

    Delete exercicio_3_soma.py

--- exercicio_3_soma.py
+++
@@ -1,6 +0,0 @@
-# Crie um programa que leia dois números e mostre a soma entre eles.
-
-num_a = int(input('Digite o primeiro número: '))
-num_b = int(input('Digite o segundo número: '))
-
-print('A soma entre {} e {} é: {} .'.format(num_a, num_b,num_a + num_b))
