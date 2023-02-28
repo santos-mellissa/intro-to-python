571fda98453fb0ccd3920d7334aba4cf0d55a5fc
Mellissa Santos <114121324+santos-mellissa@users.noreply.github.com>
2023-02-28 12:45:29 -0300

    Delete exercicio_7_media_duas_notas.py

--- exercicio_7_media_duas_notas.py
+++
@@ -1,8 +0,0 @@
-# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
-
-nota_a = int(input('Entre com a primeira nota: '))
-nota_b = int(input('Entre com a segunda nota: '))
-
-media = ((nota_a + nota_b) / 2)
-
-print('O média das notas é: {} .'.format(media))
