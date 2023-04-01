7eea89da1dd303adca80310bb8aa93479ae655fd
Mellissa Santos <114121324+santos-mellissa@users.noreply.github.com>
2023-04-01 18:21:08 -0300

    Add files via upload

---
+++ estruturas_de_controle/049_tabuada_de_um_numero.py
@@ -0,0 +1,17 @@
+"""Refaça o desafio 009 (pasta fundamentos), mostrando a tabuada de um número que o usuário escolher,
+só que agora, utilizando um laço for."""
+
+num = int(input('\nEntre com um número para saber sua tabuada: '))
+
+print(f'\n{"TABUADA":-^30}')
+print(f'{num} x 01 = {num*1}')
+print(f'{num} x 02 = {num*2}')
+print(f'{num} x 03 = {num*3}')
+print(f'{num} x 04 = {num*4}')
+print(f'{num} x 05 = {num*5}')
+print(f'{num} x 06 = {num*6}')
+print(f'{num} x 07 = {num*7}')
+print(f'{num} x 08 = {num*8}')
+print(f'{num} x 09 = {num*9}')
+print(f'{num} x 10 = {num*10}')
+print('-'*30)
