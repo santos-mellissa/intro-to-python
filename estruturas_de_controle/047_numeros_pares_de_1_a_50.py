1b0ee4b05d34b73a0966bd1016b51032c349afcb
Mellissa Santos <114121324+santos-mellissa@users.noreply.github.com>
2023-04-01 17:56:24 -0300

    Add files via upload

---
+++ estruturas_de_controle/047_numeros_pares_de_1_a_50.py
@@ -0,0 +1,8 @@
+"""Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50."""
+
+print('\n')
+
+for c in range(2,51,2):
+    print(c, end= ' ')
+print('\n')
+
